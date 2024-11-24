5a167ef0328bcecbc4554ec5e6d88f99ad17c106
slamgLuke <lucascarranzab2@gmail.com>
2024-11-24 18:09:55 -0500

    template for rtree knn

--- app/MultiDim.py
+++ app/MultiDim.py
@@ -2,6 +2,9 @@ import numpy as np
 from rtree import index
 import heapq
 
+def euclidean(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    return np.linalg.norm(a - b)
+
 
 class SequentialKNN:
     def __init__(self, collection):
@@ -13,7 +16,7 @@ class SequentialKNN:
         max_heap = []
 
         for i in range(self.collection_size):
-            dist = np.linalg.norm(query - self.collection[i])
+            dist = euclidean(query, self.collection[i])
 
             if len(max_heap) < k:
                 heapq.heappush(max_heap, (-dist, i))
@@ -30,7 +33,7 @@ class SequentialKNN:
         results = []
 
         for i in range(self.collection_size):
-            dist = np.linalg.norm(query - self.collection[i])
+            dist = euclidean(query, self.collection[i])
 
             if dist <= radius:
                 results.append((i, dist))
@@ -38,3 +41,44 @@ class SequentialKNN:
         results = sorted(results, key=lambda x: x[1])
         return results
 
+class RTreeKNN:
+    def __init__(self, collection):
+        self.collection_size = collection.shape[0]
+        p = index.Property()
+        p.dimension = collection.shape[1]
+        self.index = index.Index(properties=p)
+
+        for i in range(self.collection_size):
+            embedding = tuple(collection[i])
+            bounding_box = embedding + embedding
+            self.index.insert(i, bounding_box)
+
+
+    def knn_search(self, query, k=5):
+        query = tuple(query[0])
+        bounding_box = query + query
+        nearest_neighbors = list(self.index.nearest(bounding_box, k))
+
+        results = [(i, euclidean(np.array(query), np.array(self.index.get(i)))) for i in nearest_neighbors]
+        return sorted(results, key = lambda x: x[1])
+
+
+    def range_search(self, query, radius=5):
+        query = tuple(query[0])
+
+        # bounding box al rededeor de la query, de size = radius
+        bounding_box = tuple(
+            coord - radius if i % 2 == 0 else coord + radius
+            for i, coord in enumerate(query * 2)
+        )
+
+        candidates = list(self.index.intersection(bounding_box))
+
+        # filtrar los candidatos con ed > radius
+        results = [
+            (i, euclidean(np.array(query), np.array(self.index.get(i))))
+            for i in candidates
+            if euclidean(np.array(query), np.array(self.index.get(i))) <= radius
+        ]
+
+        return sorted(results, lambda x: x[1])

ae49f656d519d51b277c5910865ab0fd7ad09fb4
slamgLuke <lucascarranzab2@gmail.com>
2024-11-30 23:56:58 -0500

    feat: working umap dimensionality reduction

--- app/MultiDim.py
+++ app/MultiDim.py
@@ -1,14 +1,32 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
 import torch
 import torch.nn as nn
 from torchvision import models, transforms
+
 from PIL import Image
+from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
+from umap import UMAP
+
+
 from rtree import index
 import heapq
 import faiss
+
+
+# --------------------------------
+# File Paths
+# --------------------------------
+
+feature_vector_path = "data/imagenette/feature_vector.npy"
+reduced_vector_path = "data/imagenette/reduced_vector.npy"
+reducer_path = "data/imagenette/reducer.pkl"
+filenames_path = "data/imagenette/filenames.npy"
+image_folder = "data/imagenette/images"
 
 # --------------------------------
 # Model and Preprocessing
@@ -157,6 +175,7 @@ def build_vector_collection(image_folder, output_file, filename_list_file):
         output_file (str): Path to save the feature matrix.
         filename_list_file (str): Path to save the list of filenames.
     """
+    print("Building vector collection!")
     collection = []
     filenames = []
     image_files = [
@@ -179,6 +198,42 @@ def build_vector_collection(image_folder, output_file, filename_list_file):
     np.save(filename_list_file, filenames)
     print(f"Feature matrix saved to {output_file}")
     print(f"Filename list saved to {filename_list_file}")
+
+
+def reduce_collection_dimensionality(collection, reducer):
+    """
+    Reduce dimensionality of collection using reducer.
+    Args:
+        collection (np.ndarray): Embedding matrix.
+        reducer (umap / pca): dimensionality reducer with .fit_transform() method
+    """
+    print("Generating reduced dimensionality collection")
+    reduced_collection = reducer.fit_transform(collection)
+    return reduced_collection
+
+
+def reduce_query_dimensionality(query_embedding, reducer):
+    """
+    Reduce dimensionality of query embedding.
+    Args:
+        query_embedding (np.ndarray): Query embedding vector.
+        reducer (umap / pca): dimensionality reducer with .transform() method
+    """
+    reduced_embedding = reducer.transform(query_embedding.reshape(-1, 1))
+    return reduced_embedding[0]
+
+
+def load_reducer():
+    """
+    Load dimensionality reducer from file.
+    """
+    try:
+        with open(reducer_path, "rb") as f:
+            reducer = pickle.load(f)
+        return reducer
+    except Exception as e:
+        print(f"Error loading dimensionality reducer: {e}")
+        return None
 
 
 # --------------------------------
@@ -231,28 +286,35 @@ def show_query_and_neighbors(query_image_path, neighbor_paths):
 # --------------------------------
 
 def main():
-    feature_vector_path = "data/imagenette/feature_vector.npy"
-    filenames_path = "data/imagenette/filenames.npy"
-    image_folder = "data/imagenette/images"
-
     if not os.path.exists(feature_vector_path) or not os.path.exists(filenames_path):
         build_vector_collection(image_folder, feature_vector_path, filenames_path)
 
     collection = np.load(feature_vector_path)
     filenames = np.load(filenames_path)
 
+    if not os.path.exists(reduced_vector_path) or not os.path.exists(filenames_path):
+        reducer = UMAP(n_components=128)
+        reduced_collection = reduce_collection_dimensionality(collection, reducer)
+        np.save(reduced_vector_path, reduced_collection)
+        with open(reducer_path, "wb") as f:
+            pickle.dump(reducer, f)
+
+
     print(f"Collection shape: {collection.shape}")
     print(f"Number of filenames: {len(filenames)}")
 
     # visualize_embeddings(collection, filenames)
 
+    reduced_collection = np.load(reduced_vector_path)
+    reducer = load_reducer()
 
     # TESTING
-    db = RTreeKNN(collection[:-15])
+    db = RTreeKNN(reduced_collection[:-15])
     print("k-NN database built.")
 
     for i in range(1, 16):
         query_embedding = collection[-i]
+        query_embedding = reduce_query_dimensionality(query_embedding, reducer)
         query_filename = filenames[-i]
         results = db.knn_search(query_embedding, k=5)
 
